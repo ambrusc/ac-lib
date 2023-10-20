abcef3cfc29c5675c6afdd6ac3bb59b2ac1e5187
ambrus <y1qt1m9q3@mozmail.com>
2023-10-20 15:26:45 -0600

    CI runs, now let's see if it fails correctly.

--- test_all.c
+++ test_all.c
@@ -9,5 +9,6 @@ int main(int argc, char** argv) {
   (void)argv;
   ac_test_init((ac_test_opts){});
   ac_test_run(ac_test_test);
+  ac_test_fail("Seeing if CI picks up this failure.");
   return ac_test_done() ? 0 : 1;
 }
